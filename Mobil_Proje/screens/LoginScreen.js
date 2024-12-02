ebf656825cb4f52bcb51ffbf2f88a77747594af3
Emre <emreetumer0@gmail.com>
2024-12-02 16:25:37 +0300

    1.Commit
    Proje oluşturuldu ve login kısımlarına başlandı.

---
+++ Mobil_Proje/screens/LoginScreen.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AuthContent from '../components/AuthContent'
+
+export default function LoginScreen() {
+  return <AuthContent isLogin />;
+}
+const styles = StyleSheet.create({})
